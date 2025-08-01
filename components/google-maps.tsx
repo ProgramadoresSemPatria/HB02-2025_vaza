523c2c66ed67b05a74f21c872d84ac0e043cd310
Arthur Duarte <tuiduarte@gmail.com>
2025-07-31 22:08:37 -0400

    remove marker

--- components/google-maps.tsx
+++ components/google-maps.tsx
@@ -84,21 +84,6 @@ export const GoogleMaps = ({
               const countryName = countryComponent.long_name;
               const countryCode = countryComponent.short_name;
               
-              // Add a marker at the clicked location
-              const marker = new window.google.maps.Marker({
-                position: { lat, lng },
-                map: map,
-                title: countryName,
-                icon: {
-                  url: "data:image/svg+xml;charset=UTF-8," + encodeURIComponent(`
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" fill="#4285f4"/>
-                    </svg>
-                  `),
-                  scaledSize: new window.google.maps.Size(24, 24),
-                  anchor: new window.google.maps.Point(12, 24),
-                }
-              });
 
               // Set initial popover data with loading state
               const initialData: CountryData = {

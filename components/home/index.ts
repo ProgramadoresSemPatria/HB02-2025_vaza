d31c6286af7dbecc71fe87f3b3478edaf2b577e3
luiznwz <luizrenan179@gmail.com>
2025-08-03 01:42:41 -0300

    refactor: remove duplicate export of GallerySection and HeroBackgroundSection in home index file

--- components/home/index.ts
+++ components/home/index.ts
@@ -1,9 +1,8 @@
+export { GallerySection } from "./gallery-section";
 export { GlobeComponent } from "./globe-component";
 export { GlobeSection } from "./globe-section";
 export { HeroBackground } from "./hero-background";
 export { HeroHeader } from "./hero-header";
 export { PopularDestinations } from "./popular-destinations";
-export { GallerySection } from "./gallery-section";
-export { HeroBackgroundSection } from "./hero-background-section";
 export { StatsDestinationsSection } from "./stats-destinations-section";
 export { VideoExpansionSectionComponent } from "./video-expansion-section";
